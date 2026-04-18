07355aba0c52efeb2e746c372b0471115a724bdf
Per Sunde <persunde@users.noreply.github.com>
2026-04-18 17:08:51 +0900

    feat: add comments

--- src/main/kotlin/com/anagram/Anagram.kt
+++ src/main/kotlin/com/anagram/Anagram.kt
@@ -3,6 +3,7 @@ package com.anagram
 import com.anagram.io.getWordsFromFile
 
 fun findAnagrams(): Map<String, List<String>> {
+    // Group anagrams together by using the sorted characters in the word as the key
     return getWordsFromFile().groupBy { word ->
         word.toCharArray().sorted().joinToString("")
     }
--- src/main/kotlin/com/anagram/io/FileReader.kt
+++ src/main/kotlin/com/anagram/io/FileReader.kt
@@ -5,6 +5,6 @@ import java.io.File
 fun getWordsFromFile(): List<String> {
     return File("eventyr.txt")
         .readLines()
-        .flatMap { it.split(" ") }
+        .flatMap { it.split(" ") } // handle lines with multiple words on one line. Assumes that multi word lines are acceptable, even if they do not exist in the eventyr.txt file
         .filter { it.isNotBlank() }
 }
